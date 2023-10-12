fea90a00217243455ee9b724cbd65bc5ddfda8ad
celikgokhun <celik.gokhun@gmail.com>
2023-10-12 13:13:17 +0300

    add background(small)

--- site/src/jsMain/kotlin/celik/sections/MainSection.kt
+++ site/src/jsMain/kotlin/celik/sections/MainSection.kt
@@ -1,16 +1,37 @@
 package celik.sections
 
 import androidx.compose.runtime.Composable
+import celik.util.Constants.SECTION_NARROW
+import celik.util.Constants.SECTION_WIDTH
 import celik.util.Res
 import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.objectFit
 import com.varabyte.kobweb.silk.components.graphics.Image
+import org.jetbrains.compose.web.css.px
 
 @Composable
 fun MainSection() {
+    Box(
+        modifier = Modifier
+            .maxWidth(SECTION_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
+    ){
         MainBackground()
+    }
+    /*
+    Box(
+        modifier = Modifier
+            .maxWidth(SECTION_NARROW.px),
+        contentAlignment = Alignment.TopCenter
+    ){
+        LittleBackground()
+    }
+     */
 }
 
 @Composable
@@ -20,6 +41,17 @@ fun MainBackground() {
             .fillMaxSize()
             .objectFit(ObjectFit.Cover),
         src = Res.Image.background,
+        desc = "Background Image"
+    )
+}
+
+@Composable
+fun LittleBackground() {
+    Image(
+        modifier = Modifier
+            .fillMaxSize()
+            .objectFit(ObjectFit.Cover),
+        src = Res.Image.littleBack,
         desc = "Background Image"
     )
 }
--- site/src/jsMain/kotlin/celik/util/Constants.kt
+++ site/src/jsMain/kotlin/celik/util/Constants.kt
@@ -1,7 +1,14 @@
 package celik.util
 
+object Constants {
+    const val SECTION_WIDTH = 1920
+    const val SECTION_NARROW = 420
+}
+
+
 object Res {
     object Image {
         const val background = "background.svg"
+        const val littleBack = "littleBack.svg"
     }
 }
